3873e752a8214c4cfa21fa1065d2aef1af62661b
Lenilson Teixeira <lenilsonts@gmail.com>
2022-02-19 18:14:17 -0300

    feat: add server and database config

--- main.go
+++ main.go
@@ -1,5 +1,15 @@
 package main
 
+import (
+	"github.com/LenilsonTeixeira/books-api/database"
+	"github.com/LenilsonTeixeira/books-api/server"
+)
+
 func main() {
+	database.StartDB()
+
+	server := server.NewServer()
+
+	server.Run()
 
 }
